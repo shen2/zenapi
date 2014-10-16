c587483bcce3c5536f17f718c0d1083f0243ead9
cuebyte <cuebit.w@gmail.com>
2014-10-16 13:56:59 +0800

    added Instagram

---
+++ src/InstagramOAuth2.php
@@ -0,0 +1,32 @@
+<?php
+namespace ZenOAuth2;
+
+class InstagramOAuth2 extends OAuth2Abstract {
+	/**
+	 * Set up the API root URL.
+	 *
+	 * @ignore
+	 */
+    public $host = 'https://api.instagram.com/v1/';
+
+	/**
+	 * Set API URLS
+	 */
+	/**
+	 * @ignore
+	 */
+	public function accessTokenURL()  { return 'https://api.instagram.com/oauth/access_token/'; }
+	/**
+	 * @ignore
+	 */
+	public function authorizeURL()    { return 'https://api.instagram.com/oauth/authorize/'; }
+
+	protected function _tokenFilter($response){
+		$token = json_decode($response, true);
+		if (!is_array($token) || isset($token['error'])) {
+			var_dump($response);var_dump($params);var_dump($token);	//modified by shen2, 用来调试
+			throw new Exception("get access token failed." . $token['error']);
+		}
+		return $token;
+	}
+}
