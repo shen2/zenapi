5821142277289a65ef779d42a9f077ca7e0b2180
cuebyte <cuebit.w@gmail.com>
2014-10-23 19:51:23 +0800

    modify

--- src/InstagramClient.php
+++ src/InstagramClient.php
@@ -3,22 +3,9 @@ namespace ZenOAuth2;
 
 class InstagramClient extends Client{
 	
-	public $host = 'https://api.instagram.com/v1/';
-	
-	public function parseResponse($response){
-		if ($this->format === 'json' && $this->decode_json) {
-			$json = json_decode($response);		//	解析成对象而不是关联数组
-			if ($json !== null)
-				return $json;
-		}
-		return $response;
-	}
+	public $host = 'https://api.instagram.com/';
 	
 	protected function _paramsFilter(&$params){
 		$params['key'] = $this->access_token;
 	}
-
-    protected function _getUserId(){
-
-    }
 }
